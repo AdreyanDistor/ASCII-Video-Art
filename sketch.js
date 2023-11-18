3d782dbc2b3f14bd4cef5ca4749d2e52efad2a5c
AdreyanDistor <adreyandistor@gmail.com>
2023-11-18 11:31:55 -0800

    w

--- sketch.js
+++ sketch.js
@@ -1,7 +1,38 @@
+const ascii_characters = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`'.                            ";
+
+let video;
+let asciiDiv;
+
 function setup() {
+  noCanvas();
+  video = createCapture(VIDEO);
+  video.size(48, 48);
   createCanvas(400, 400);
+  asciiDiv = createDiv();
 }
 
 function draw() {
-  background(220);
+  video.loadPixels();
+  let asciiVideo = "";
+  for (let i = 0; i < video.height; i++) {
+    for (let j = 0; j < video.width; j++) {
+      let pixelIndex = (i + j * video.width) * 4;
+      let r = video.pixels[pixelIndex + 0];
+      let g = video.pixels[pixelIndex + 1];
+      let b = video.pixels[pixelIndex + 2];
+      let avg = (r + g + b) / 3;
+      let len = ascii_characters.length;
+      let charIndex = floor(map(avg, 0, 255, 0, len));
+
+      let character = ascii_characters.charAt(charIndex);
+      console.log(character);
+      if (character == ' ') {
+        asciiVideo += "&nbsp;";
+      } else {
+        asciiVideo += character;
+      }
+    }
+    asciiVideo += '<br/>';
+  }
+  asciiDiv.html(asciiVideo);
 }
