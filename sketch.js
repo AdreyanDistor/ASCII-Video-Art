5a283757f87fc6406d18d751c105ed27deda482f
lazylain <inmylain@gmail.com>
2023-11-18 17:19:45 -0800

    completed dino quest screen

--- sketch.js
+++ sketch.js
@@ -1,39 +1,42 @@
-const ascii_characters = ".,~-:;!*$@8%0O  ";
+const ascii_characters = "abcdefg ";
 
 let video;
-let asciiDiv;
+let asciiDiv
 
 function setup() {
   noCanvas();
-  frameRate(24)
-  video = createVideo(VIDEO);
-  video.size(48, 48);
+  frameRate(12);
+  video = createVideo("dinoVid480.mp4");
+  video.size(video.width*2, video.height*2);
   asciiDiv = createDiv();
-  
+  video.hide();
+  video.play();
+  video.loop();
 }
 
 function draw() {
   video.loadPixels();
   let asciiVideo = "";
-  for (let y = 0; y < video.height; y++) {
-    for (let x = 0; x < video.width; x++) {
-      let pixelIndex = (x + (y * video.width) ) * 4;
+
+  for (let j = 0; j < video.height; j++) {
+    for (let i = 0; i < video.width; i++) {
+
+      let pixelIndex = (i + j * video.width) * 4;
       let r = video.pixels[pixelIndex + 0];
       let g = video.pixels[pixelIndex + 1];
       let b = video.pixels[pixelIndex + 2];
       let avg = (r + g + b) / 3;
       let len = ascii_characters.length;
-      let charIndex = floor(map(avg, 0, 255, 0, len));
-
-      let character = ascii_characters.charAt(charIndex);
-      console.log(character);
-      if (character == ' ') {
+      let charIndex = floor(map(avg, 0, 255, 0, len-1));
+      // document.write(ascii_characters[charIndex]);
+      if (ascii_characters[charIndex] == ' ') {
         asciiVideo += "&nbsp;";
       } else {
-        asciiVideo += character;
+        asciiVideo += ascii_characters[charIndex];
       }
     }
     asciiVideo += '<br/>';
   }
+  asciiDiv.center();
   asciiDiv.html(asciiVideo);
 }
